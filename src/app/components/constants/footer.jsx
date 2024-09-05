2b9418c0e098fc705e25d179fb711195bb7accc4
ashukla333 <amanshukla3747@gmail.com>
2024-09-05 20:06:53 +0530

    setup done

--- src/app/components/constants/footer.jsx
+++ src/app/components/constants/footer.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 const Footer = () => {
   const router=useRouter()
   return (
-    <footer className="bg-white text-primary-black-color drop-shadow-2xl shadow-2xl  dark:text-gray-300 py-6 px-10 flex justify-between items-center">
+    <footer className="bg-white text-primary-black-color drop-shadow-2xl shadow-2xl  dark:text-gray-300 py-6 px-10 flex justify-between md:flex-row text-center flex-col items-center">
       {/* Theme Toggle */}
       <div className="flex items-center cursor-pointer  "  onClick={()=>{router.push("/")}}>
       <MainLogo className="text-2xl text-black dark:text-white" />
